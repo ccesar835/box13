1a6e4eb018062ef6bd0f4d2f30dd1287439a8486
cj835 <ccesar835@hotmail.com>
2025-02-05 15:20:45 +0000

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const logo = document.querySelector("#hero .logo");
+
+    function adjustLogoSize() {
+        const heroHeight = document.querySelector("#hero").offsetHeight;
+        if(logo) {
+            logo.style.maxHeight = (heroHeight * 0.5) + "px";
+        }
+    }
+    if (logo) {
+        adjustLogoSize();
+        window.addEventListener("resize", adjustLogoSize);
+    }
+    
+    // Smooth scrolling for older browsers
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}

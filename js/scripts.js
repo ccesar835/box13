debbbda0b38f96307285a80defcc89e534a1322e
cj835 <ccesar835@hotmail.com>
2025-02-11 11:45:37 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,27 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         });
     });
-     // JavaScript to Rotate Image on Scroll
-     document.addEventListener('scroll', function() {
-        const image = document.querySelector('.rotating-image');
-        const imageRect = image.getBoundingClientRect();
-        const viewportHeight = window.innerHeight;
-    
-        // Calculate the middle point of the viewport
-        const middleOfViewport = viewportHeight / 2;
-    
-        // Calculate the middle point of the image
-        const middleOfImage = imageRect.top + (imageRect.height / 2);
-    
-        // Calculate rotation based on how far the image's middle is from the viewport's middle
-        const distanceToMiddle = Math.abs(middleOfViewport - middleOfImage);
-        const maxDistance = viewportHeight / 2;
-    
-        // Rotate the image based on its distance to the middle of the viewport
-        const rotation = Math.min((distanceToMiddle / maxDistance) * 180, 180);
-        image.style.transform = `rotate(${rotation}deg)`;
-    });
-  });
+});
 
 function scrollToTop() {
     window.scrollTo({
